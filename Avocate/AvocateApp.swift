48d6e9b561b051930b44675444935d8d42c10c76
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-06-16 00:50:20 -0300

    Initial Commit

---
+++ Avocate/AvocateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocateApp.swift
+//  Avocate
+//
+//  Created by Manny Alvarez on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AvocateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
